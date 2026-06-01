27fdf90f462e46a082188b8f8a920bcb73e0bcf0
shivampratapsingh464 <shivampratapsingh464@gmail.com>
2026-06-02 01:47:06 +0530

    full stack upload

--- hotel-booking-server/server.js
+++ hotel-booking-server/server.js
@@ -1,11 +1,12 @@
-const roomRoutes = require("./routes/roomRoutes");
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 
 const connectDB = require("./config/db");
+
 const authRoutes = require("./routes/authRoutes");
 const bookingRoutes = require("./routes/bookingRoutes");
+const roomRoutes = require("./routes/roomRoutes");
 
 const app = express();
 
@@ -13,10 +14,10 @@ connectDB();
 
 app.use(cors());
 app.use(express.json());
-app.use("/api/rooms", roomRoutes);
 
 app.use("/api/auth", authRoutes);
 app.use("/api/bookings", bookingRoutes);
+app.use("/api/rooms", roomRoutes);
 
 app.get("/", (req, res) => {
   res.send("Hotel Booking API Running...");
@@ -24,6 +25,6 @@ app.get("/", (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
